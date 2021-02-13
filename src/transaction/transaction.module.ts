1e5b422360c5e04359cd73ed9e05f067a8fe20c0
sandi.agung <sandi.agung@ecampus.ut.ac.id>
2021-02-13 21:07:29 +0700

    Ready

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { TransactionEntity } from './transaction.entity';
+import { TransactionService } from './transaction.service';
+import { TransactionResolver } from './transaction.resolver';
 @Module({
     imports: [
         TypeOrmModule.forFeature([TransactionEntity])
     ],
     providers: [
+        TransactionService,
+        TransactionResolver
     ]
 })
 export class TransactionModule {}
